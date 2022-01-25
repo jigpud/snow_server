4188a129da881e583765ff3415d25dd459a6ec12
jigpud <jigpud@qq.com>
2022-01-25 11:19:30 +0800

    [init] 初始化项目
    Signed-off-by: jigpud <jigpud@qq.com>

---
+++ src/main/java/com/jigpud/snow/SnowApplication.java
@@ -0,0 +1,11 @@
+package com.jigpud.snow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SnowApplication.class, args);
+	}
+}
